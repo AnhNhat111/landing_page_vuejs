2957fc7cf61f75317c181eb964a049bfbb94b187
nhat-r-digital <nhat@r-digital.tech>
2023-03-10 15:47:12 +0700

    firt commit

---
+++ src/views/Master.vue
@@ -0,0 +1,22 @@
+<template>  
+    <header-section></header-section>
+    <router-view></router-view>
+    <later-section></later-section>
+    <footer-section></footer-section>
+</template>
+
+<script>
+import HeaderSection from '../components/fontend/Header.vue'
+import FooterSection from '../components/fontend/Footer.vue';
+import LaterSection from '../components/fontend/Newsletter.vue'
+export default {
+  components : {
+    HeaderSection, 
+    LaterSection,
+    FooterSection
+},
+  name: "Master",
+};
+</script>
+
+<style></style>
